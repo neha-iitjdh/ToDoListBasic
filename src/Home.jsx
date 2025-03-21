b9cc09ecd3b5c5ae12706b03e723977cc56c50d1
Neha Sharma <neha.iitjdh@gmail.com>
2025-03-21 18:09:21 +0530

    Add Home and Create Component

---
+++ src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Create from './Create'
+
+function Home() {
+  return (
+    <div><h2>Todo List</h2>
+    <Create/>
+    </div>
+  )
+}
+          
+export default Home
